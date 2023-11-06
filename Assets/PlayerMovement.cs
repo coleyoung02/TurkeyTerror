209bb1b8aa48b2a2e3268dd69f661b597bd87cab
coleyoung02 <coleyoung741@gmail.com>
2023-11-05 20:52:53 -0800

    moving player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,32 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed = 1;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 movementDir = new Vector2(0, 0);
-        if (Input.GetKey("W"))
+        if (Input.GetKey(KeyCode.W))
         {
             movementDir += new Vector2(0, 1);
         }
-        if (Input.GetKey("A"))
+        if (Input.GetKey(KeyCode.A))
         {
             movementDir += new Vector2(-1, 0);
         }
-        if (Input.GetKey("S"))
+        if (Input.GetKey(KeyCode.S))
         {
             movementDir += new Vector2(0, -1);
         }
-        if (Input.GetKey("D"))
+        if (Input.GetKey(KeyCode.D))
         {
             movementDir += new Vector2(1, 0);
         }
-
+        rb.velocity = movementDir.normalized * speed;
     }
 }
