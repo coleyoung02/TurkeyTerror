e156eea38e9e5f5b698cea9d4737d7455dd4bc26
coleyoung02 <coleyoung741@gmail.com>
2023-11-07 23:41:21 -0800

    fix win state - master

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,17 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Pickup : MonoBehaviour
 {
+    private static int pickedUp = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Turkey"))
         {
-            Destroy(gameObject);
+            pickedUp++;
+            gameObject.SetActive(false);
         }
     }
 
-    private void OnDestroy()
+    private void OnDisable()
     {
-        if (FindObjectsByType<Pickup>(FindObjectsSortMode.InstanceID).Length <= 0)
+        if (pickedUp == 4)
         {
             SceneManager.LoadScene("WinScene");
         }
